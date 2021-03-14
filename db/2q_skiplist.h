b324931a516421571748dc90b5464a07724c4cfc
spiceandwolf <fangboyangchina@gmail.com>
2021-03-14 22:28:40 +0800

    开始编写Insert_Twoqueue()

--- db/2q_skiplist.h
+++ db/2q_skiplist.h
@@ -6,6 +6,7 @@
 #include <cstdlib>
 
 #include "db/skiplist.h"
+#include "db/dbformat.h"
 
 namespace leveldb
 {
@@ -28,6 +29,12 @@ namespace leveldb
         void Insert(const Key& key);
         //Contains()函数没有变化
         //Iterator已有的功能不需要变化
+
+        //将数据插入2q队列
+        void Insert_Twoqueue(const Twoqueue_Node*& node, const bool& is_same);
+
+        int RandomHeight();
+        bool Equal(const Key& a, const Key& b) const { return (compare_(a, b) == 0); }
 
     private:
         enum { kMaxHeight = 12};
@@ -57,6 +64,8 @@ namespace leveldb
 
         std::atomic<int> max_height_;//同skiplist
         Random rnd_;//同skiplist
+
+        InternalKeyComparator icmp_;
     };
     
     template <typename Key, class Comparator>
@@ -72,9 +81,8 @@ namespace leveldb
             return next_[n].load(std::memory_order_acquire);
         }
 
-        Twoqueue_Node* Newer(int n) {
-            assert(n >= 0);
-            return newer_[n].load(std::memory_order_acquire);
+        Twoqueue_Node* Follow() {
+            return follow_.load(std::memory_order_acquire);
         }
 
         void SetNext(int n, Twoqueue_Node* x) {
@@ -82,9 +90,9 @@ namespace leveldb
             next_[n].store(x, std::memory_order_release);
         }
 
-        void SetNewer(int n, Twoqueue_Node* x) {
+        void SetFollow(std::Twoqueue_Node* x) {
             assert(n >= 0);
-            newer_[n].store(x, std::memory_order_release);
+            follow_.store(x, std::memory_order_release);
         }
 
         //在某些线程安全的情况下使用
@@ -93,9 +101,8 @@ namespace leveldb
             return next_[n].load(std::memory_order_relaxed);
         }
 
-        Twoqueue_Node* NoBarrier_Newer(int n) {
-            assert(n >= 0);
-            return newer_[n].load(std::memory_order_relaxed);
+        Twoqueue_Node* NoBarrier_Follow() {
+            return follow_.load(std::memory_order_relaxed);
         }
 
         void NoBarrier_SetNext(int n, Twoqueue_Node* x) {
@@ -103,14 +110,14 @@ namespace leveldb
             next_[n].store(x, std::memory_order_relaxed);
         }
 
-        void NoBarrier_SetNewer(int n, Twoqueue_Node* x) {
-            assert(n >= 0);
-            newer_[n].store(x, std::memory_order_relaxed);
+        void NoBarrier_SetFollow(Twoqueue_Node* x) {
+            follow_.store(x, std::memory_order_relaxed);
         }
 
         private:
         std::atomic<Twoqueue_Node*> next_[1];//在skiplist中的下一个
-        std::atomic<Twoqueue_Node*> newer_[1];//在2q中的下一个
+        std::atomic<Twoqueue_Node*> follow_;//在2q中FIFO顺序的下一值
+        std::atomic<Twoqueue_Node*> new_;//在2q中的同一关键字的新的值
     };
 
     template <typename Key, class Comparator>
@@ -121,6 +128,18 @@ namespace leveldb
             sizeof(Twoqueue_Node) + sizeof(std::atomic<Twoqueue_Node*>) * (height - 1)
         );
         return new (node_memorey) Twoqueue_Node(key);
+    }
+
+    template <typename Key, class Comparator>
+    int Twoqueue_SkipList<Key, Comparator>::RandomHeight() {
+        static const unsigned int kBranching = 4;
+        int height = 1;
+        while (height < kMaxHeight && ((rnd_.Next() % kBranching) == 0)) {
+            height++;
+        }
+        assert(height > 0);
+        assert(height <= kMaxHeight);
+        return height;
     }
 
     template <typename Key, class Comparator>
@@ -199,11 +218,12 @@ namespace leveldb
     head_(NewTwoqueue_Node(0, kMaxHeight)),
     normal_head_(head_),
     cold_head_(nullptr),
-    cur_scan_node_(head_), 
-    cur_cold_node_(nullptr),
+    cur_scan_node_(head_),
     cur_node_(head_),
+    cur_cold_node_(nullptr),
     max_height_(1), 
-    rnd_(0xdeadbeef) {
+    rnd_(0xdeadbeef),
+    icmp_(cmp) {
         for (int i = 0; i < kMaxHeight; i++) {
             head_->SetNext(i, nullptr);
         }
@@ -211,7 +231,44 @@ namespace leveldb
     
     template <typename Key, class Comparator>
     void Twoqueue_SkipList<Key, Comparator>::Insert(const Key& key) {
+        Twoqueue_Node* prev[kMaxHeight];
+        Twoqueue_Node* x = FindGreaterOrEqual(key, prev);
 
+        bool is_same = true;
+
+        //将节点插入2q链表中,比较新插入的节点的userkey和与它紧邻的前一userkey,
+        //如果是相同的userkey,则将该节点添加到前一节点的new_指针上，
+        //否则添加到follow_指针上
+
+        assert(x == nullptr || !Equal(key, x->key));
+
+        int height = RandomHeight();
+        //为新增的层初始化
+        if (height > GetMaxHeight()) {
+            for (int i = GetMaxHeight(); i < height; i++) {
+                prev[i] = head_;
+            }
+            max_height_.store(height, std::memory_order_relaxed);
+        }
+        
+        //插入skiplist
+        x = NewTwoqueue_Node(key, height);
+        for (int i = 0; i < height; i++) {
+            x->NoBarrier_SetNext(i, prev[i]->NoBarrier_Next(i));
+            prev[i]->SetNext(i, x);
+        }
+        
+        //插入2q链表
+        Insert_Twoqueue(x, is_same);
+    }
+
+    //根据is_same判断插入new_还是follow_
+    template <typename Key, class Comparator>
+    void Twoqueue_SkipList<Key, Comparator>::Insert_Twoqueue(const Twoqueue_Node*& node, const bool& is_same) {
+        
+        cur_node_->SetFollow(node);
+        cur_node_ = node;
+        
     }
 
     /*
